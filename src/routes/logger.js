8a7a540d7b4b81a93e86c8f6d6839ff44a770f15
talha01rahman <talha01rahman@gmail.com>
2022-06-02 23:14:26 +0530

    hey

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+const endpoint = "https://www.functionup.com"
+
+const log = function(){
+    console.log('I am inside the log function')
+}
+
+module.exports.url = endpoint
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const res = require('express/lib/response');
+const logger = require('./logger')
+
+
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    console.log('The constant in logger route has a value '+logger.url)
+    res.send('My first API')
 });
-
+router.get('/test-me1', function (req, res) {
+    console.log('Thr constant in logger route has a value '+endpoint)
+    res.send('My 2nd API')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('My 3rd API')
+});
+router.get('/test-me3', function (req, res) {
+    res.send('My 4th API')
+});
+router.get('/test-me4', function (req, res) {
+    res.send('My 5th API')
+});
+router.get('/test-me5', function (req, res) {
+    res.send('My 6th API')
+});
 module.exports = router;
 // adding this comment for no reason
