0425b57db8fc767f65d789856c66e0feb680ac5b
talha01rahman <talha01rahman@gmail.com>
2022-06-02 23:13:48 +0530

    hey

---
+++ src/util/helper.js
@@ -0,0 +1,10 @@
+const underscore = require('underscore')
+const printDate = function()  {
+    let currentDate = new Date()
+    console.log(currentDate)
+}
+
+
+
+
+module.exports.printDate = printDate
