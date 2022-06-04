c1e985d5f3ee0825b2291af303acf0e175bf340a
talha01rahman <talha01rahman@gmail.com>
2022-06-04 16:07:40 +0530

    updated mondb atlas id w talha01rahman

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,7 +6,11 @@ const UserController= require("../controllers/userController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
+router.post("/created", async function (req,res){
+    let data = req.body
+    let saveData = await UserModel.create(data)
+    res.send({msg:saveData})
+})
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
