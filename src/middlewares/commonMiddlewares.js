c37a58d79b71f5078684cf403b3dc10bb3d11f50
talha01rahman <talha01rahman@gmail.com>
2022-06-13 20:16:29 +0530

    hey

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,11 @@
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+const mid= function ( req, res, next) {
+    let data = req.headers;
+
+    if(!req.headers.isfreeappuser) return res.send({msg:"request is missing a mandatory header"})
+    if(!(req.headers.isfreeappuser=="true" || req.headers.isfreeappuser=="false") return res.send(msg:"this is not header of isfreeappuser")
     next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
-
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
-
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+module.exports.mid= mid
